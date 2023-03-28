6ab0065775d9718504df85c10627e94f4f2e7fe4
markinh00 <jmcoutinhonunes@gmail.com>
2023-03-28 15:44:34 -0300

    second commit

--- main.py
+++ main.py
@@ -5,8 +5,57 @@ db = Database(database="dex", collection="pokemons")
 db.resetDatabase()
 
 def getPokemonByDex(number: int):
-    return db.collection.find({"id": number})
+    foundPokemon = db.collection.find_one({"id": number})
 
+    if not foundPokemon:
+        print("Dados não encontrados!!")
+        return None
+
+    writeAJson(foundPokemon, foundPokemon["name"]["english"].lower())
+    return foundPokemon
+
+def getPokemonBytype(type):
+    foundPokemon = db.collection.find({"type": type})
+
+    if not foundPokemon:
+        print("Dados não encontrados!!")
+        return None
+
+    writeAJson(foundPokemon, f"pokemon_{type.lower()}")
+    return foundPokemon
+
+def getPokemonByName(name):
+    foundPokemon = db.collection.find_one({"name.english": {"$eq": name}})
+
+    if not foundPokemon:
+        print("Dados não encontrados!!")
+        return None
+
+    writeAJson(foundPokemon, f"{name.lower()}")
+    return foundPokemon
+
+def getPokemonByAttack(operator, value: int):
+    foundPokemon = db.collection.find({"base.Attack": {f"${operator}": value}})
+
+    if not foundPokemon:
+        print("Dados não encontrados!!")
+        return None
+
+    writeAJson(foundPokemon, f"attack_{operator.lower()}_{value}")
+    return foundPokemon
+
+def getPokemonByHP(operator, value: int):
+    foundPokemon = db.collection.find({"base.HP": {f"${operator}": value}})
+
+    if not foundPokemon:
+        print("Dados não encontrados!!")
+        return None
+
+    writeAJson(foundPokemon, f"HP_{operator.lower()}_{value}")
+    return foundPokemon
 
 bulbasaur = getPokemonByDex(1)
-writeAJson(bulbasaur, "bulbasaur")
+grass = getPokemonBytype("Grass")
+charmander = getPokemonByName("Charmander")
+attackGte50 = getPokemonByAttack("gte", 50)
+HPLte50 = getPokemonByHP("lte", 50)
